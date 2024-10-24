3d8b3e2ec7322e84ce45caa253524f227e7627f7
Eddie822 <eddiezarazua822@gmail.com>
2024-10-23 18:12:27 -0600

    eeyeyeyeyeyeyeye

--- resources/js/bootstrap.js
+++ resources/js/bootstrap.js
@@ -2,3 +2,6 @@ import axios from 'axios';
 window.axios = axios;
 
 window.axios.defaults.headers.common['X-Requested-With'] = 'XMLHttpRequest';
+// resources/js/app.js
+import 'bootstrap';
+
--- routes/web.php
+++ routes/web.php
@@ -5,3 +5,9 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return view('welcome');
 });
+
+// En routes/web.php
+Route::get('/inicio', function () {
+    return view('index');
+});
+
